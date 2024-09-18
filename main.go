271ec87e1d89c8ecd4e04f86de3dad5d1d45adbb
cagrigit-hub <cagrikutayok@hotmail.com>
2024-09-18 20:04:58 +0300

    basic user

--- main.go
+++ main.go
@@ -1,10 +1,29 @@
 package main
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/Elys-SaaS/auth/router"
+	echoSwagger "github.com/swaggo/echo-swagger"
+)
 
+// @title Swagger Example API
+// @version 1.0
+// @description Conduit API
+// @title Conduit API
+
+// @host 127.0.0.1:8585
+// @BasePath /api
+
+// @schemes http https
+// @produce	application/json
+// @consumes application/json
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
 func main() {
-	e := echo.New()
+	r := router.New()
 
-	e.Logger.Fatal(e.Start(":1323"))
+	r.GET("/swagger/*", echoSwagger.WrapHandler)
 
+	r.Logger.Fatal(r.Start("127.0.0.1:8585"))
 }
